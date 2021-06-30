01162dbc3234001fc567211ea72e3fccf9739005
Matheus Melo <matheus.correia@lavid.ufpb.br>
2021-06-30 14:12:03 -0300

    nearest neighbor to be finished

--- main.py
+++ main.py
@@ -17,9 +17,25 @@ def plot_graph(graph):
 
 
 def describe_edges(graph):
-    edges = g.es()
+    edges = graph.es()
     for each in edges:
         print('Edge {} with cost {}'.format(each.tuple, each['cost']))
+
+
+def nearest_neighbor(graph):
+    vertex_list = graph.vs()
+    edges_list = graph.es()
+    solution = []
+    for vertex in vertex_list:
+        vertex_edges = edges_list.select(_source=vertex.index)
+        nearer = edges_list[0]['cost']
+        s = vertex_edges.select(cost_lt=nearer)
+        if len(s) > 0:
+            print(min(s['cost']))
+        # print("Vertex {} edges are".format(vertex.index))
+        # for each in vertex_edges:
+        #     print('Edge {} with cost {}'.format(each.tuple, each['cost']))
+
 
 
 if __name__ == '__main__':
@@ -28,6 +44,8 @@ if __name__ == '__main__':
     df = pd.read_fwf(instance, sep=" ", skiprows=3, header=None)
 
     g = Graph.Weighted_Adjacency(df.to_numpy(), 'undirected', attr='cost', loops=False)
+
+    nearest_neighbor(g)
 
     # describe_edges(g)
     # plot_graph(g)
