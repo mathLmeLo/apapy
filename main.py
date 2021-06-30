6502cb42e57079bcea843df650a43efef06cdbdf
Matheus Melo <matheus.correia@lavid.ufpb.br>
2021-06-30 13:26:17 -0300

    plot_graph and describe_edges functions

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ def get_dim_p(filename):
     arq = open(filename, 'r')
     lines = arq.readlines()
     arq.close()
-    dim = int(lines[0].split(' ')[1])
-    p = int(lines[1].split(' ')[1])
-    return dim, p
+    d_ = int(lines[0].split(' ')[1])
+    p_ = int(lines[1].split(' ')[1])
+    return d_, p_
+
+
+def plot_graph(graph):
+    lyt = graph.layout("kk")
+    plot(graph, layout=lyt)
+
+
+def describe_edges(graph):
+    edges = g.es()
+    for each in edges:
+        print('Edge {} with cost {}'.format(each.tuple, each['cost']))
 
 
 if __name__ == '__main__':
@@ -17,7 +28,6 @@ if __name__ == '__main__':
     df = pd.read_fwf(instance, sep=" ", skiprows=3, header=None)
 
     g = Graph.Weighted_Adjacency(df.to_numpy(), 'undirected', attr='cost', loops=False)
-    print(g)
 
-    layout = g.layout("kk")
-    plot(g, layout=layout)
+    # describe_edges(g)
+    # plot_graph(g)
